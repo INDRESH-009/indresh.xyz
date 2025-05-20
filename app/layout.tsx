838f98a4246e7bfc31f5a63dbba3d15c5ec1adb4
Indresh <indreshmr12345@gmail.com>
2025-05-20 21:25:45 +0530

    complete reset

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 // app/layout.tsx
 import './globals.css'
-import { NavBar } from '@/components/Navbar'
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        
         {children}
       </body>
     </html>
