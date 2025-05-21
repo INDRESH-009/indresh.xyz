a2f855a89377329bfa21c501ec33c81c607198b7
Indresh <indreshmr12345@gmail.com>
2025-05-21 15:25:24 +0530

    homepage styling

--- app/page.js
+++ app/page.js
@@ -46,13 +46,13 @@ const sections = [
 ];
 export default function Home() {
   return (
-    <section>
-      <div>
-        <h1>My personal Journal</h1>
+    <section className="pt-16 pb-24">
+      <div className="text-center max-w-2xl mx-auto px-4">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">My personal Journal</h1>
         <p>A space for my brain to talk to the universe</p>
       </div>
 
-      <div>
+      <div  className="grid gap-6 px-4 sm:grid-cols-2 lg:grid-cols-3 max-w-5xl mx-auto p-6">
         {sections.map((sec)=>(
           <SectionCard key={sec.slug} {...sec} />
         ))}
--- components/SectionCard.js
+++ components/SectionCard.js
@@ -2,10 +2,19 @@ import Link from 'next/link';
 export default function Sectioncard({slug,title,description}){
     return(
         <Link href={`/${slug}`} className='block'>
-            <div className='border border-grey-200 rounded-2xl p-6hover:shadow-lg transition-shadow h-full flex flex-col'>
+            <div className='border border-grey-200 rounded-2xl p-6hover:shadow-lg transition-shadow h-full flex flex-col p-6'>
                 <h2 className='text-xl font-semibold text-gray-900 mb-2'>{title}</h2>
                 <p className='text-gray-600 flex-grow'>{description}</p>
-                <span className='mt-4 text-sm text-gray-500'>→</span>
+                <span className='mt-4 text-sm text-gray-500 flex justify-center'>
+                    <button className="relative border p-2 rounded-2xl w-40 overflow-hidden group hover:cursor-pointer">
+                        {/* Background element */}
+                        <span className="absolute left-0 top-0 w-0 h-full bg-gray-800 transition-all duration-300 group-hover:w-full"></span>
+                        {/* Button text */}
+                        <span className="relative transition-colors duration-300 group-hover:text-white">
+                            Read
+                        </span>
+                    </button>
+                </span>
             </div>
         
         </Link>
