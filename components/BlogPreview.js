3f79deb18cc8c4779996920297127474ce2afd6d
Indresh <indreshmr12345@gmail.com>
2025-05-21 16:15:27 +0530

    blog preview component created

---
+++ components/BlogPreview.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+export default function BlogPreview({section,slug,date,time,excerpt}){
+    const day = new Date(date).getDate().toString().padStart(2,'0');
+    const month = new Date(date).toLocaleString('default',{month:'short'});
+    return(
+        <Link href={`/${section}/${slug}`} className="flex items-start space-x-4 py-6">
+            <div className="text-4xl font-serif text-gray-900 w-16 text-center">
+                <div>{day}</div>
+                <div className="text-sm text-gray-500 uppercase">{month}</div>
+            </div>
+            <div>
+                <h3 className="text-xl font-semibold text-gray-800 mb-1">{title}</h3>
+                <p className="text-gray-600 mb-1">{excerpt}</p>
+            </div>
+        </Link>
+    )
+
+}
