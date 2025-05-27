a6f5ca1d5024b5ba8cee8be54283293fada79ae1
Indresh <indreshmr12345@gmail.com>
2025-05-27 09:45:33 +0530

    mdx image issue fixed

--- components/MDXContent.js
+++ components/MDXContent.js
@@ -1,9 +1,14 @@
 'use client'
 import {useMemo} from 'react'
 import {getMDXComponent} from 'mdx-bundler/client'
+import Image from 'next/image';
+
+const components = {
+    Image,
+}
 
 export default function MDXContent({code}){
     //convert the bundled code viz a string to a react component
     const Component = useMemo(() => getMDXComponent(code), [code]);
-    return <Component/>;
+    return <Component components={components}/>;
 }
